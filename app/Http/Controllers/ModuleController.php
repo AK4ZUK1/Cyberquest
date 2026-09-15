ce9d0f207d98dacfeb7e730c587b14ade33d0429
Syazwan <syazwandw@gmail.com>
2026-09-15 15:31:03 +0800

    Use native Supabase public object URL for module PDFs

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -33,10 +33,10 @@ class ModuleController extends Controller
             $fileName = time() . '_' . preg_replace('/\s+/', '_', $file->getClientOriginalName());
             
             // Upload to Supabase Storage 'modules' bucket
-            $path = $file->storeAs('uploads/pdfs', $fileName, 'supabase');
-            
-            // Generate public cloud URL
-            $pdfUrl = Storage::disk('supabase')->url($path);
+            $path = $file->storeAs('uploads/pdfs', $filename, 'supabase');
+
+            // Generate the correct native Supabase public URL
+            $pdfUrl = "https://wothtkwgjnqjrmrweqzh.supabase.co/storage/v1/object/public/modules/" . $path;
         }
 
         Module::create([

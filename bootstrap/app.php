31df53e579aa0df11b176b023c14c19b125089a9
Syazwan <syazwandw@gmail.com>
2026-09-18 14:56:53 +0800

    updated bootsrap with proxy trusting integrated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Trust all proxies (required for Render, Heroku, Cloudflare, etc.)
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
